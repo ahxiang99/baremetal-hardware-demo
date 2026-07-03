41b05da7ce29200374964be06693785c4c1b1b08
zhixiang <ahxiang63@gmail.com>
2026-07-03 15:24:38 +0800

    Add Changing Mode into Log.

--- Inc/AppMode.hpp
+++ Inc/AppMode.hpp
@@ -3,7 +3,20 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "logger.hpp"
+
 enum class AppMode : uint8_t { SendPacket = 0, Console = 1, CliMode = 2, COUNT = 3 };
+
+struct AppMode_Table {
+    AppMode     mode;
+    const char* name;
+};
+
+static const AppMode_Table namingTable[static_cast<uint8_t>(AppMode::COUNT)] = {
+    {AppMode::SendPacket, "Packet Mode" },
+    {AppMode::Console,    "Console Mode"},
+    {AppMode::CliMode,    "CLI Mode"    }
+};
 
 static inline void onButtonPress(void* ctx) {
     AppMode curMode = static_cast<std::atomic<AppMode>*>(ctx)->load(std::memory_order_relaxed);
@@ -13,5 +26,6 @@ static inline void onButtonPress(void* ctx) {
         uint8_t i = static_cast<uint8_t>(curMode) + 1;
         curMode   = static_cast<AppMode>(i);
     }
+    LOG_INFO("Changing Mode: {}", namingTable[static_cast<uint8_t>(curMode)].name);
     static_cast<std::atomic<AppMode>*>(ctx)->store(curMode, std::memory_order_relaxed);
 }
