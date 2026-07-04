f480846322f10c28d0a06cbff4f1cd194432d9af
zhixiang <ahxiang63@gmail.com>
2026-07-04 13:36:57 +0800

    Make Template to allow compiler to deduce suitable type

--- drivers/common/RegisterUtils.hpp
+++ drivers/common/RegisterUtils.hpp
@@ -2,16 +2,19 @@
 
 #include <cstdint>
 
+#include "low-level/nvic.h"
+
 namespace RegisterUtils {
-inline void modify(volatile uint32_t& reg, uint32_t clearMask, uint32_t setMask) {
+template <typename T1, typename T2>
+inline void modify(volatile T1& reg, T2 clearMask, T2 setMask) {
     reg = (reg & ~clearMask) | setMask;
 }
-
-inline void setBits(volatile uint32_t& reg, uint32_t Mask) {
+template <typename T1, typename T2>
+inline void setBits(volatile T1& reg, T2 Mask) {
     reg |= Mask;
 }
-
-inline void clearBits(volatile uint32_t& reg, uint32_t Mask) {
+template <typename T1, typename T2>
+inline void clearBits(volatile T1& reg, T2 Mask) {
     reg &= ~Mask;
 }
 }  // namespace RegisterUtils
