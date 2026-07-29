c3b8301e8a9582f6917e0d31bbd8cba8dcc3b831
zhixiang <ahxiang63@gmail.com>
2026-07-29 21:28:41 +0800

    Add bitmask for SysTick

--- drivers/systick/low-level/systick.h
+++ drivers/systick/low-level/systick.h
@@ -3,21 +3,26 @@
 
 #include <stdint.h>
 
-#define SCB_BASE 0xE000E000U
+#define SCB_BASE       0xE000E000U
 #define SYSTICK_OFFSET 0x0010U
-#define SYSTICK_BASE (SCB_BASE + SYSTICK_OFFSET)
+#define SYSTICK_BASE   (SCB_BASE + SYSTICK_OFFSET)
 
-#define __IOM volatile  // Read Write Structure member permissions
-#define __OM volatile   // Write Only Structure member permissions
-#define __IM volatile   // Read Only Structure member permissions
+#define __IOM volatile // Read Write Structure member permissions
+#define __OM  volatile // Write Only Structure member permissions
+#define __IM  volatile // Read Only Structure member permissions
 
 typedef struct {
-    __IOM uint32_t CTRL;  /*!< SysTick Control and Status Register, Address offset: 0x00 */
-    __IOM uint32_t LOAD;  /*!< SysTick Reload Value Register,       Address offset: 0x04 */
-    __IOM uint32_t VAL;   /*!< SysTick Current Value Register,      Address offset: 0x08 */
-    __IOM uint32_t CALIB; /*!< SysTick Calibration Value Register,  Address offset: 0x0C */
+	__IOM uint32_t CTRL;  /*!< SysTick Control and Status Register, Address offset: 0x00 */
+	__IOM uint32_t LOAD;  /*!< SysTick Reload Value Register,       Address offset: 0x04 */
+	__IOM uint32_t VAL;   /*!< SysTick Current Value Register,      Address offset: 0x08 */
+	__IOM uint32_t CALIB; /*!< SysTick Calibration Value Register,  Address offset: 0x0C */
 } SysTick_TypeDef;
 
-#define SysTickPtr ((SysTick_TypeDef*)SYSTICK_BASE)
+#define SysTickPtr ((SysTick_TypeDef *)SYSTICK_BASE)
+
+#define SYST_CSR_ENABLE    (1 << 0)
+#define SYST_CSR_TICKINT   (1 << 1)
+#define SYST_CSR_CLKSOURCE (1 << 2)
+#define SYST_CSR_COUNTFLAG (1 << 16)
 
 #endif /* SYSTICK_H */
